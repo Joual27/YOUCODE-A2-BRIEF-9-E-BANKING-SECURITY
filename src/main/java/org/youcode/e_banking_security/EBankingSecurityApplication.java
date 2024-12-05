de324dcb71a694e1657a9458eae2123c74c36407
Joual27 <endy.panda@Outlook.com>
2024-12-05 10:11:44 +0100

    project config , docker setup

---
+++ src/main/java/org/youcode/e_banking_security/EBankingSecurityApplication.java
@@ -0,0 +1,11 @@
+package org.youcode.e_banking_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingSecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EBankingSecurityApplication.class, args);
+	}
+}
---
+++ src/test/java/org/youcode/e_banking_security/EBankingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youcode.e_banking_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingSecurityApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
